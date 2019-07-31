187f4032535e11eaae6ddee2584a1e6beeff716d
Adalberto Henrique Campaner <adalberto.campaner@gmail.com>
2019-07-31 14:07:15 -0400

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/adalba/cursomc/CursomcApplication.java
+++ src/main/java/com/adalba/cursomc/CursomcApplication.java
@@ -1,25 +1,17 @@
 package com.adalba.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
+import com.adalba.cursomc.domain.*;
+import com.adalba.cursomc.domain.enums.EstadoPagamento;
+import com.adalba.cursomc.repositores.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.adalba.cursomc.domain.Categoria;
-import com.adalba.cursomc.domain.Cidade;
-import com.adalba.cursomc.domain.Cliente;
-import com.adalba.cursomc.domain.Endereco;
-import com.adalba.cursomc.domain.Estado;
-import com.adalba.cursomc.domain.Produto;
 import com.adalba.cursomc.domain.enums.TipoCliente;
-import com.adalba.cursomc.repositores.CategoriaRepository;
-import com.adalba.cursomc.repositores.CidadeRepository;
-import com.adalba.cursomc.repositores.ClienteRepository;
-import com.adalba.cursomc.repositores.EnderecoRepository;
-import com.adalba.cursomc.repositores.EstadoRepository;
-import com.adalba.cursomc.repositores.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -36,6 +28,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +81,22 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
---
+++ src/main/java/com/adalba/cursomc/repositores/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.adalba.cursomc.repositores;
+
+import com.adalba.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
